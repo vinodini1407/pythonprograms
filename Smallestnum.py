22d17d3f1580fec29e511714ebb85cfeaaf1cb0e
vinodini1407 <vinodinikoduru@gmail.com>
2025-03-17 16:00:31 +0530

    Create Smallestnum.py

---
+++ Smallestnum.py
@@ -0,0 +1,9 @@
+a=int(input())
+b=int(input())
+c=int(input())
+if(a<=b and a<=c):
+    print("a is smaller")
+elif(b<=a and b<=c):
+    print ("b is smaller")
+elif(c<=a and c<=b):
+    print ("c is smaller")
